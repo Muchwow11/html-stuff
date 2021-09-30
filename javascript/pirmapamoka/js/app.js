ae3bd4e3f2d574ba9c0972b100e898d728517bbd
Tajus <tajus02@gmail.com>
2021-09-30 11:30:31 +0300

    javascript

---
+++ javascript/pirmapamoka/js/app.js
@@ -0,0 +1,172 @@
+// var name = "Dominykas papaausis"
+// console.log(name.length)
+// console.log(name[6])
+// console.log(name.indexOf("papa"))
+// console.log(name.slice(5,10))
+// var changedName = name.replace("Dom", "Lop")
+//
+// console.log(changedName.toUpperCase())
+// console.log(changedName)
+//
+// let items = ['telefonas', 'ssd', 'pele'];
+// items.push('kilimelis')
+// console.log(items[3])
+// console.log("prekiu kiekis: "+items.length)
+// let price = "2000, 3000, 4000, 5000"
+// price = price.split(',')
+// console.log(price)
+//
+// let sequince = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+// // splice delete
+// /*
+// sequince.splice(6, 1,)
+// console.log(sequince.length)
+// console.log(sequince)
+//  */
+// for (var i = 0; i < sequince.length; i++){
+//     console.log(sequince[i]);
+// }
+//
+// let difData = ['saskaita', 795, [0, 1, 2]];
+// difData = difData.join(',')
+// console.log(difData)
+// console.log("skaicius: " +difData.length +typeof (difData))
+//
+//
+// let userName = "muchwow11";
+// if (userName){
+//     console.log('true')
+// }else{
+//     console.log('false')
+// }
+
+// var names = ['Indre', 'Tajus', 'Donatas', 'Tomas']
+
+// for (var i = 0; i < names.length; i++) {
+//     if (names[i] === 'Tajus') {
+//         console.log('Kontaktas surastas: ')
+//         console.log(names[i]);
+//         break;
+//     }
+// }
+
+// in duos indexa, of duos masyvo reiksme
+// for (var name in names){
+//     console.log(names[name])
+// }
+
+
+
+let names = [];
+var count = 5;
+for (var i = 0; i < count; i++) {
+    names.push(prompt("Ivesk varda"));
+    if (names == "Tajus"){
+        names.unshift
+        names.splice(0, 1)
+        console.log('not allowed')
+
+    }
+}
+
+
+
+console.log("sveciu sarasas:")
+for (var name in names){
+
+    console.log(names[name])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var name = "name"
+
+if(name){
+// alert("hello world");
+    let name = "Name"
+}else {
+    let name = "Name1"
+}
+console.log(typeof (data))
+/*
+*/
+
+
+/* function printer(){
+}
+/* var name = "Tajus";
+var age = 18;
+var data = ['Tajus', 'Marius', 'Indre', 'Donatas']
+var areYouHere = true;
+var human{
+    hands: 2;
+}
+
+// primityvai
+console.log(typeof(name))
+console.log(typeof(age))
+* multi line comment
+*/
+// single line comment
+/*
+let atlyginimas = 2000
+let lastName = "Tajus";
+console.log("mano vardas yra " +lastName+ " ir mano alga yra " +atlyginimas+ "eur per menesi")
+
+
+*/
