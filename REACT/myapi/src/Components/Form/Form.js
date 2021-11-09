b431afb3f1f60932458bce9496477f25597c5da0
Tajus <tajus02@gmail.com>
2021-11-09 15:14:27 +0200

    reactPAINPAINPAIN123

--- REACT/myapi/src/Components/Form/Form.js
+++ REACT/myapi/src/Components/Form/Form.js
@@ -2,41 +2,21 @@ import React, {useState} from "react";
 
 function  Form(){
 
-    const [title,setTitle] = useState()
     const [movie,setMovie] = useState()
 
-    const titleHandler = (e)=>{
-        setTitle(e.target.title)
-    }
-
-    const submitHandler = (e) =>{
+    const getData = (e) =>{
         e.preventDefault()
-    }
-
-    const getData = () =>{
-        fetch(`http://www.omdbapi.com/?t=${title}&apikey=7115894f`)
+        fetch(`http://www.omdbapi.com/?t=${movie}&apikey=7115894f`)
             .then(response => response.json)
             .then(data => setMovie(data))
 
-        console.log(movie)
     }
-    // const submitHandler =(e)=>{
-    //     const GetInfo=()=>{
-    //
-    //     fetch(`http://www.omdbapi.com/${movie}&apikey=7115894f`)
-    //         .then(response => response.json)
-    //         .then(data => setMovie(data))
-    // }
-    //     e.preventDefault()
-    //
-    //     return GetInfo
-    // }
     return  (<div>
 
 
         <form>
-          <div className="form-group"><input className="form-control inp" type="text" placeholder="Type in Movie" onChange={titleHandler}/></div>
-            <button className={"btn btn-primary"} onSubmit={submitHandler} onClick={getData} >Search</button>
+          <div className="form-group"><input className="form-control inp" type="text" placeholder="Type in Movie" /></div>
+            <button className={"btn btn-primary"} onClick={getData} >Search</button>
         </form>
     </div>)
 }
